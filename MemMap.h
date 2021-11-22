17ae40f9047b908cb58a870ed8b33959f4c35d3a
Andrew Tieszen <pilottzn@gmail.com>
2021-11-22 07:53:29 -0700

    Test Mem Map

--- MemMap.h
+++ MemMap.h
@@ -8,17 +8,17 @@ private:
 	uint8_t crt [0xbfe0]; // Cartridge ROM, RAM, and memory mappers
 
 public:
-	uint8_t readMem(uint16_t addr) {
+	uint8_t read(uint16_t addr) {
 		if (addr <= 0x0fff) return ram[addr % 0x0800];
 		else if (addr <= 0x3fff) return ppu[(addr - 0x2000) % 0x0008];
 		else if (addr <= 0x401f) return apu[addr - 0x4000];
-		//else return crt[addr - 0x4020];
+		else return crt[addr - 0x4020];
 	}
 
-	void writeMem(uint16_t addr, uint8_t value) {
+	void write(uint16_t addr, uint8_t value) {
 		if (addr <= 0x0fff) ram[addr % 0x0800] = value;
 		else if (addr <= 0x3fff) ppu[(addr - 0x2000) % 0x0008] = value;
 		else if (addr <= 0x401f) apu[addr - 0x4000] = value;
-		//else crt[addr - 0x4020] = value;
+		else crt[addr - 0x4020] = value;
 	}
 };
